0f9de28ae60e6cb52452c9c7b76e8e33e8e5f750
Pavan Shah <pavanshah77@gmail.com>
2016-12-09 00:05:30 -0800

    Initial commit with messages, surveys and mongodb integration

--- HelpMyUsersClient/app.js
+++ HelpMyUsersClient/app.js
@@ -7,7 +7,9 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path');
+var mongo = require("./routes/mongo");
 var mainLogic = require('./routes/mainLogic');
+var mongoConnectURL = "mongodb://pavanshah77:pavanshah77@ds129028.mlab.com:29028/helpmyusersdatabase";
 
 var app = express();
 
@@ -29,6 +31,9 @@ if ('development' == app.get('env')) {
 
 app.get('/', mainLogic.loadHomePage);
 
-http.createServer(app).listen(app.get('port'), function(){
+mongo.connect(mongoConnectURL, function(){
+  console.log('Connected to mongo at: ' + mongoConnectURL);
+  http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+  }); 
 });
