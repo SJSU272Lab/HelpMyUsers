2bb6fa6f30f5b9c037f65e3a29c83dd3ba070e02
Pavan Shah <pavanshah77@gmail.com>
2016-12-11 21:14:10 -0800

    With analytics for users

--- HelpMyUsersClient/routes/mongo.js
+++ HelpMyUsersClient/routes/mongo.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var db;
 var connected = false;
+
 exports.connect = function(url, callback){
     MongoClient.connect(url, function(err, _db){
       if (err) { throw new Error('Could not connect: '+err); }
@@ -10,6 +11,7 @@ exports.connect = function(url, callback){
       callback(db);
     });
 };
+
 exports.collection = function(name){
     if (!connected) {
       throw new Error('Must connect to Mongo before calling "collection"');
